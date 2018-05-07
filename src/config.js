64db88ce7a5200ff80c727e34a85d1804a7e6c98
Guanlun Zhao <guanlunzhao@gmail.com>
2018-05-07 13:57:29 -0500

    changed ip

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 import { ENV } from './env';
 
-export const SERVER_HOST = (ENV === 'prod') ? '18.218.172.215' : 'localhost';
+export const SERVER_HOST = (ENV === 'prod') ? '18.220.167.147' : 'localhost';
